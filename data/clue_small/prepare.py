caab5bb80e4c44312f277fc25b699cb54412157a
Hao Sun <nagisa940216@gmail.com>
2023-02-17 22:13:50 +0800

    fix generators for all

--- data/clue_small/prepare.py
+++ data/clue_small/prepare.py
@@ -1,6 +1,7 @@
 import os
 import json
 import pdb
+from itertools import chain
 
 import numpy as np
 from datasets import Dataset
@@ -25,7 +26,33 @@ def wiki_generator():
                             yield {'text': json.loads(line)['text'].strip().split('\n\n', 2)[-1]}
 
 
-dataset = Dataset.from_generator(wiki_generator)
+def web_text_generator():
+    path = 'data/clue_small/webtext2019zh'
+    files = ['web_text_zh_train.json', 'web_text_zh_valid.json', 'web_text_zh_testa.json']
+    for f_name in files:
+        with open(os.path.join(path, f_name), 'r') as f:
+            for line in f.readlines():
+                j = json.loads(line)
+                yield {'text': f"{j['title']}{j['desc']}[SEP]{j['content']}"}
+
+
+def news_generator():
+    path = 'data/clue_small/webtext2019zh'
+    files = ['news2016zh_train.json', 'news2016zh_valid.json']
+    for f_name in files:
+        with open(os.path.join(path, f_name), 'r') as f:
+            for line in f.readlines():
+                j = json.loads(line)
+                yield {'text': j['content']}
+
+
+def generate_all():
+    yield from wiki_generator()
+    yield from web_text_generator()
+    yield from news_generator()
+
+
+dataset = Dataset.from_generator(generate_all)
 tokenizer = BertTokenizer.from_pretrained("uer/gpt2-chinese-cluecorpussmall")
 
 dataset = dataset.train_test_split(test_size=0.001, seed=2357, shuffle=True)
@@ -35,7 +62,6 @@ split_dataset['val'] = split_dataset.pop('test')  # rename the test split to val
 
 def process(example):
     ids = tokenizer.encode(example['text'])
-    ids.append(tokenizer.sep_token_id)
     out = {'ids': ids, 'len': len(ids)}
     return out
 
